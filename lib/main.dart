c1faeafb7fa037e25ec23347a5e418c89d2bacf5
Your Name <sahlasagu01@gmail.com>
2025-03-13 10:36:41 +0530

    community is finished

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 
 import 'package:device_preview/device_preview.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -11,26 +12,27 @@ import 'features/bottom_navigation/profile/profile_page.dart';
 import 'features/login/login_page.dart';
 import 'features/onboarding_screen/onboarding_page1.dart';
 import 'features/onboarding_screen/splash_screen.dart';
+import 'firebase_options.dart';
 late double height;
 late double width;
-// Future<void> main() async {
-//   WidgetsFlutterBinding.ensureInitialized();
-//   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-//   runApp(DevicePreview(
-//       enabled: kDebugMode,
-//       tools: const [
-//         ...DevicePreview.defaultTools,
-//       ],
-//       builder: (context) =>  EventAPP()));
-// }
-void main(){
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(DevicePreview(
-    enabled: kDebugMode,
+      enabled: kDebugMode,
       tools: const [
         ...DevicePreview.defaultTools,
       ],
       builder: (context) =>  EventAPP()));
 }
+// void main(){
+//   runApp(DevicePreview(
+//     enabled: kDebugMode,
+//       tools: const [
+//         ...DevicePreview.defaultTools,
+//       ],
+//       builder: (context) =>  EventAPP()));
+// }
 class EventAPP extends StatelessWidget {
   const EventAPP({super.key});
 
